e80133e14d5bc3f1be910e777261a4bd57f6c7e5
Eliezer Martinez <develiezermartinez@gmail.com>
2025-05-13 13:23:22 -0600

    update a vite.config para builds limpios

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,14 +12,19 @@ export default defineConfig({
     },
   },
   // Asegurarse de que Vite sirva correctamente los archivos de la carpeta public
-  publicDir: 'public',
-  base: '/credenciales/',  // Ruta base coincidente con Nginx
+  publicDir: "public",
+  base: "/credenciales/", // Ruta base coincidente con Nginx
+
+  // 👇 Nueva configuración para el build
+  build: {
+    outDir: "dist/credenciales", // Genera todo dentro de /dist/credenciales/
+    emptyOutDir: true, // Limpia el directorio antes de cada build
+  },
 
   // Configuración de resolución de rutas
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+});
