0d7446f5ffbeec6cf3a6092923a6ecb2e67d19c9
Eliezer Martinez <develiezermartinez@gmail.com>
2025-05-13 13:20:18 -0600

    fix route models faceapi

--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -29,7 +29,7 @@ const WebcamCapture = ({ onPhotoValidated }) => {
 
   useEffect(() => {
     const loadModels = async () => {
-      const MODEL_URL = "/models";
+     const MODEL_URL = "./models";
       try {
         await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
         await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
